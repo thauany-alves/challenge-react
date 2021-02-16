c45ef8cc8a5281ef17b650b9fae8a42a9b93e1c3
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-02-15 22:30:26 -0300

    Mapeando estados

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import Header from './HeaderComponent';
 import Product from './ProductsComponent';
-import {PRODUCTS} from '../shared/products';
+import Cart from './CartComponent';
+import Item from './ItemComponent';
+// import {PRODUCTS} from '../shared/products';
+import {Switch, Route, withRouter} from 'react-router-dom';
+import { addItemToCart } from '../redux/ActionCreators';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+  return {
+    products: state.products,
+    items_cart: state.items_cart
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addItemToCart: (productId) => dispatch(addItemToCart(productId))
+});
 
 
 class Main extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = { products: PRODUCTS}
-  }
+   
 
   render(){
     return(
       <React.Fragment>
         <Header />
-        <Product products={this.state.products} />
+        <Switch>
+          <Route exact path="/"  component = {() => <Product products={this.props.products} addItemToCart={this.props.addItemToCart} />}  />
+          <Route exact path="/cart" component = {() => <Cart items_cart={this.props.items_cart} />}  />
+        </Switch>
       </React.Fragment>
     );
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
