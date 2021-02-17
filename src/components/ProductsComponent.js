40585e0e4dec5bffe08e6d827d268099d54d60ac
thauany-alves <thauanyaraujoalves@hotmail.com>
2021-02-16 21:52:51 -0300

    Adicionando ordenamento

--- src/components/ProductsComponent.js
+++ src/components/ProductsComponent.js
@@ -1,8 +1,9 @@
-import React, {Component} from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 
-const CardsContainer = styled.div`
+
+const Container = styled.div`
   max-width: 800px;
   margin: 0 auto;
   display: grid;
@@ -10,29 +11,70 @@ const CardsContainer = styled.div`
   gap: 20px;
 `;
 
-const UnstyledLink = styled(Link)`
-  text-decoration: none;
+const OptionSort = styled.button`
+  margin: 10px 0;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.1);
+  border: 1px solid aliceblue;
+  border-radius: 4px;
+  font-size: 16px;
+  font-weight: 600;
+
   color: aliceblue;
+
+  cursor: pointer;
+
+  &:hover{
+    color: var(--color-secondary);
+    border-color: var(--color-secondary);
+  }
+
+`;
+
+const Button = styled.button`
+  margin: 8px;
+  padding: 10px;
+  border-radius: 4px;
+  font-weight: medium;
+  border: 2px solid ${props => props.primary ? "var(--color-secondary)" : "var(--color-primary)"};
+  /* background-color: var(--color-secondary); */
+  font-size: 16px;
+  color:   ${props => props.primary ? "white" : "var(--color-primary)"};
+  background: ${props => props.primary ? "var(--color-secondary)" : "white"};
+  cursor: pointer;
+  
 `;
 
 const Card = styled.div`
-  background-color: #7159c1;
+  background-color: var(--color-primary);
+  border-radius: 4px;
+  font-weight: 600;
+  overflow: hidden;
+
+  transition: width 2s;
+
+  &:hover {
+  width: 110%;
+}
 `;
 
 const ProductName = styled.div`
-  padding: 24px;
+  height: 36px;
+  padding: 8px;
+  margin-bottom: 4px;
 `;
 
 const ProductInfo = styled.div`
+  padding: 0 8px;
   display: grid;
   grid-template-columns: 1fr 1fr;
 `;
 
 const Price = styled.p`
-  background-color: #7159c1;
+  background-color: var(--color-primary);
   filter: brightness(90%);
-  padding: 5px 20px;
-  border-radius: 20px;
+  padding: 5px;
+  border-radius: 10px;
   text-align: center;
 `;
 
@@ -40,47 +82,81 @@ const Img = styled.img`
   width:100%;
 `;
 
-class Products extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {selectedItem: null};
-    }
+export default function Products(props) {
 
-    onItemSelect(item) {
-      this.setState({ selectedItem: item});
-    }
+  const products = props.products;
+    
+  const history = useHistory();
   
-    render(){
-      const products = this.props.products.map(product => {
-        return (
-          <div key={product.id}>
-              <Card>
-                <UnstyledLink to={`/products/${product.id}`} >
-                  <div>
-                    <Img src={`../assets/${product.image}`} alt="Product" />
-                  </div>
-            
-                  <ProductName>
-                    <p>{product.name}</p>
-                  </ProductName>
-            
-                  <ProductInfo>
-                    <p>{product.score}</p>
-                    <Price>{`R$ ${product.price}`}</Price>
-                  </ProductInfo>
-                </UnstyledLink>
-              </Card>
-          </div>
-        );
-      });
+  function handleAddToCart({product}){
+    props.addItemToCart(product);
+    history.push('/cart');
+  }
 
-      return(
-        <CardsContainer>
-          {products}
-        </CardsContainer>
+  function RenderProducts(){
+    return products.map(product => {
+      return (
+        <div key={product.id}>
+          
+            <Card>
+              <div>
+                <Img src={`../assets/${product.image}`} alt="Product" />
+              </div>
+              <ProductName>
+                <p>{product.name}</p>
+              </ProductName>
+              <ProductInfo>
+                <Price>{`R$ ${product.price}`}</Price>
+                <p><i className="fa fa-star fa-lg"></i> {product.score}</p>
+              </ProductInfo>
+              <Button primary onClick={() => handleAddToCart({product})}>
+                <span><i className="fa fa-shopping-cart fa-lg"></i> Adicionar ao Carrinho</span>
+              </Button>
+            </Card>
+        </div>
       );
-    }
+    });
+  }
+  
+  function handleSortScore(){
+    products.sort(function(a,b) {
+      return b.score -a.score;
+    });
+    history.push("/");
+  }
+
+  function handleSortPrice(){
+    products.sort(function(a,b) {
+      return a.price - b.price;
+    });
+    history.push("/");
+  }
+
+  function handleSortAlp(){
+    products.sort(function(a,b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+    history.push("/");
+  }
+  
+    return(
+      <>
+        <Container>
+          <OptionSort onClick={handleSortScore}>Maior Score</OptionSort>
+          <OptionSort onClick={handleSortPrice}>Menor Preço</OptionSort>
+          <OptionSort onClick={handleSortAlp}>Ordem Alfabética</OptionSort>
+        </Container>
+        <Container>
+          <RenderProducts />
+        </Container>
+      </>
+    );
     
 }
-
-export default Products;
